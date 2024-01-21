b24f29ac9f4154fc95e6889eb49f584cc0049b2c
JayFiji <fkafkaandthem@gmail.com>
2024-01-21 17:22:51 -0300

    Creación de user test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -9,5 +10,7 @@ mongoose.connect(process.env.MONGO)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log(err));
 
-
 app.listen(3000, () => console.log('Server listening on port 3000'));
+
+app.use('/api/user', userRouter);
+
