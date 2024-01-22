67f077eabc3ee04e265177bfe3e39b8036372fec
JayFiji <fkafkaandthem@gmail.com>
2024-01-22 16:34:09 -0300

    Completa la página de creación de cuenta de nuevo usuario

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Crear una cuenta</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='Nombre de usuario'
+        className='border p-3 rounded-lg' id='username'/>
+        <input type="text" placeholder='correo electrónico'
+        className='border p-3 rounded-lg' id='email'/>
+        <input type="text" placeholder='contraseña'
+        className='border p-3 rounded-lg' id='password'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Registrarme</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Ya tenés una cuenta?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Iniciar sesión</span>
+        </Link>
+      </div>
+    </div>
   )
 }
