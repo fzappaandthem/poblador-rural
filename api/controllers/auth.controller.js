203de5fdb920ff00ba20361977e9339fa8f67666
JayFiji <fkafkaandthem@gmail.com>
2024-01-27 22:55:02 -0300

    Creación de la api route para el inicio de sesión

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
+import  { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -11,6 +13,23 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.json({message: "Usuario creado"});
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne( {email} );
+        if(!validUser) return next(errorHandler(404, "Usuario no encontrado"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(404, "Contraseña incorrecta"));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: pass, ...otherFields} = validUser._doc;
+        res.cookie("token", token, {
+            httpOnly: true
+        }).status(200).json(otherFields);
     } catch (error) {
         next(error);
     }
