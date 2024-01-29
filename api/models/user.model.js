5af3ed3099bfface2a937fd94a128d2ae846a2af
JayFiji <fkafkaandthem@gmail.com>
2024-01-29 17:29:22 -0300

    configurado firebase, conectado con la aplicación para storage

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required:true,
     },
+    avatar: {
+        type: String,
+        default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg',
+    }
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "poblador-rural.firebaseapp.com",
+  projectId: "poblador-rural",
+  storageBucket: "poblador-rural.appspot.com",
+  messagingSenderId: "61157702823",
+  appId: "1:61157702823:web:5dd28cac181c852e9d6f82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
