c74b0d8a6b78319e8c6ccb1c713d116738205c10
JayFiji <fkafkaandthem@gmail.com>
2024-02-04 08:27:47 -0300

    Creación de update user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,7 +27,7 @@ export const signin = async (req, res, next) => {
         if(!validPassword) return next(errorHandler(404, "Contraseña incorrecta"));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         const {password: pass, ...otherFields} = validUser._doc;
-        res.cookie("token", token, {
+        res.cookie("access_token", token, {
             httpOnly: true
         }).status(200).json(otherFields);
     } catch (error) {
