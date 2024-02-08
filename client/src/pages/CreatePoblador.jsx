2cd152975e921fc4ed62d361aa4006fb427d6e09
JayFiji <fkafkaandthem@gmail.com>
2024-02-08 06:14:29 -0300

    correcciones en la UI de creación de mensajes al poblador

--- client/src/pages/CreatePoblador.jsx
+++ client/src/pages/CreatePoblador.jsx
@@ -32,7 +32,7 @@ export default function CreatePoblador() {
                     id='repeats'
                     min='1'
                     max='1000'
-                    placeholder='1'
+                    value='3'
                     required
                 />
           </div>
@@ -41,7 +41,7 @@ export default function CreatePoblador() {
             <div className='flex gap-6 flex-wrap'>
                 <div className='flex gap-1'>
                 <input type='checkbox' id='Mon0700' className='w-5' />
-                <span>7</span>
+                <span>07:00</span>
                 </div>
                 <div className='flex gap-1'>
                 <input type='checkbox' id='Mon1130' className='w-5' />
@@ -49,7 +49,7 @@ export default function CreatePoblador() {
                 </div>
                 <div className='flex gap-1'>
                 <input type='checkbox' id='Mon1300' className='w-5' />
-                <span>13</span>
+                <span>13:00</span>
                 </div>
                 <div className='flex gap-1'>
                 <input type='checkbox' id='Mon1630' className='w-5' />
@@ -57,11 +57,11 @@ export default function CreatePoblador() {
                 </div>
                 <div className='flex gap-1'>
                 <input type='checkbox' id='Mon1900' className='w-5' />
-                <span>19</span>
+                <span>19:00</span>
                 </div>
                 <div className='flex gap-1'>
                 <input type='checkbox' id='Mon2100' className='w-5' />
-                <span>21</span>
+                <span>21:00</span>
                 </div>
             </div>
           </div>
@@ -69,7 +69,7 @@ export default function CreatePoblador() {
             <span className='font-semibold w-20'>Martes</span>
             <div className='flex gap-1'>
               <input type='checkbox' id='Tue0700' className='w-5' />
-              <span>7</span>
+              <span>07:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Tue1130' className='w-5' />
@@ -77,7 +77,7 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Tue1300' className='w-5' />
-              <span>13</span>
+              <span>13:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Tue1630' className='w-5' />
@@ -85,18 +85,18 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Tue1900' className='w-5' />
-              <span>19</span>
+              <span>19:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Tue2100' className='w-5' />
-              <span>21</span>
+              <span>21:00</span>
             </div>
           </div>
           <div className='flex gap-6 flex-wrap justify-start'>
             <span className='font-semibold w-20'>Miercoles</span>
             <div className='flex gap-1'>
               <input type='checkbox' id='Wed0700' className='w-5' />
-              <span>7</span>
+              <span>07:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Wed1130' className='w-5' />
@@ -104,7 +104,7 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Wed1300' className='w-5' />
-              <span>13</span>
+              <span>13:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Wed1630' className='w-5' />
@@ -112,18 +112,18 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Wed1900' className='w-5' />
-              <span>19</span>
+              <span>19:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Wed2100' className='w-5' />
-              <span>21</span>
+              <span>21:00</span>
             </div>
           </div>
           <div className='flex gap-6 flex-wrap justify-start'>
             <span className='font-semibold w-20'>Jueves</span>
             <div className='flex gap-1'>
               <input type='checkbox' id='Thu0700' className='w-5' />
-              <span>7</span>
+              <span>07:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Thu1130' className='w-5' />
@@ -131,7 +131,7 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Thu1300' className='w-5' />
-              <span>13</span>
+              <span>13:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Thu1630' className='w-5' />
@@ -139,18 +139,18 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Thu1900' className='w-5' />
-              <span>19</span>
+              <span>19:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Thu2100' className='w-5' />
-              <span>21</span>
+              <span>21:00</span>
             </div>
           </div>
           <div className='flex gap-6 flex-wrap justify-start'>
             <span className='font-semibold  w-20'>Viernes</span>
             <div className='flex gap-1'>
               <input type='checkbox' id='Fri0700' className='w-5' />
-              <span>7</span>
+              <span>07:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Fri1130' className='w-5' />
@@ -158,7 +158,7 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Fri1300' className='w-5' />
-              <span>13</span>
+              <span>13:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Fri1630' className='w-5' />
@@ -166,18 +166,18 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Fri1900' className='w-5' />
-              <span>19</span>
+              <span>19:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Fri2100' className='w-5' />
-              <span>21</span>
+              <span>21:00</span>
             </div>
           </div>
           <div className='flex gap-6 flex-wrap justify-start'>
             <span className='font-semibold w-20'>Sábado</span>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sat0700' className='w-5' />
-              <span>7</span>
+              <span>07:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sat1130' className='w-5' />
@@ -185,7 +185,7 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sat1300' className='w-5' />
-              <span>13</span>
+              <span>13:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sat1630' className='w-5' />
@@ -193,18 +193,18 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sat1900' className='w-5' />
-              <span>19</span>
+              <span>19:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sat2100' className='w-5' />
-              <span>21</span>
+              <span>21:00</span>
             </div>
           </div>
           <div className='flex gap-6 flex-wrap justify-start'>
             <span className='font-semibold w-20'>Domingo</span>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sun0700' className='w-5' />
-              <span>7</span>
+              <span>07:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sun1130' className='w-5' />
@@ -212,15 +212,15 @@ export default function CreatePoblador() {
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sun1300' className='w-5' />
-              <span>13</span>
+              <span>13:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sun1900' className='w-5' />
-              <span>19</span>
+              <span>19:00</span>
             </div>
             <div className='flex gap-1'>
               <input type='checkbox' id='Sun2100' className='w-5' />
-              <span>21</span>
+              <span>21:00</span>
             </div>
           </div>
         </div>
