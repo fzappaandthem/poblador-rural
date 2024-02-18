8baf48bd3b317aa949427d19406ffb40a4599f70
JayFiji <fkafkaandthem@gmail.com>
2024-02-17 21:53:04 -0300

    Creación de api route para obtener los mensajes al poblador de usuario logueado

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,10 +1,24 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import {errorHandler} from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 export const test = (req, res) => {
     res.json({message: "user api route is working"});
 }
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'Sólo podes ver tus propios mensajes al poblador!'));
+  }
+};
 
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id)
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser, getUserListings } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 
 export default router;
