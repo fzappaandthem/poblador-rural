f5205e09be27143cb556fee11bbbc0ea2157e062
JayFiji <fkafkaandthem@gmail.com>
2024-02-17 23:00:30 -0300

    Creación del api route para la edición de mensaje al poblador

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,31 @@
 import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Mensaje al poblador no encontrado!'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'Sólo podés editar los mensajes al poblador creados por vos!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
