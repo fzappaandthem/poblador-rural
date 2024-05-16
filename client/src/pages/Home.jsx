9cfe45925b327d1b12a48e2a3cc261f4dac273af
JayFiji <fkafkaandthem@gmail.com>
2024-05-16 16:00:58 -0300

    chequea si fehca ultima emisión es mayoir que proxima emisión menos 1 minuto, en lugar de menos 3 horas

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -90,7 +90,7 @@ export default function Home() {
 
                 <div className="flex w-full flex-col gap-4 autofill:bg-slate-100">
                   {
-                    listings.filter(l => new Date (l.fechaUltimaEmision) >= fechaEmisionProx - 3 * 60 * 60 * 1000 ).map((listing) => (
+                    listings.filter(l => new Date (l.fechaUltimaEmision) >= fechaEmisionProx - 1 * 60 * 1000 ).map((listing) => (
                       console.log(`fecha ultima emisión\n ${listing.fechaUltimaEmision} >= ${fechaEmisionProx} : \n ${listing}`)
                     ))
                   }
