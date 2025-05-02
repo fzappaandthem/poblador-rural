8db796c0c3ce7ca865aace705b53cb18f13b671e
JayFiji <fkafkaandthem@gmail.com>
2025-05-01 23:54:51 -0300

    min-h-[180px] era demasiado, lo pasé a min-h-[80px]

--- client/src/pages/ListingItem.jsx
+++ client/src/pages/ListingItem.jsx
@@ -4,7 +4,7 @@ import { FcCollaboration } from "react-icons/fc";
 
 export default function ListingItem({listing}) {
   return (
-    <div className={`shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-[330px] md:w-[660px] min-h-[180px] ${
+    <div className={`shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-[330px] md:w-[660px] min-h-[80px] ${
         listing.category === 'misas'
           ? 'bg-orange-100'
           : 'bg-white' // default
@@ -12,7 +12,7 @@ export default function ListingItem({listing}) {
         <Link to={`/update-poblador/${listing._id}`}>
             <FcCollaboration className='mt-2 ml-2 h-4 w-4 text-green-700'/>
             <div className="p-3 flex flex-col gap-2 w-full">
-                <p className='text-lg font-semibold w-[330px] md:w-[660px] min-h-[180px] text-slate-700'>{listing.recipient}</p>
+                <p className='text-lg font-semibold w-[330px] md:w-[660px] min-h-[80px] text-slate-700'>{listing.recipient}</p>
                 <div className="flex items-center gap-1">
                     <p className='text-sm text-gray-600 w-full'>{listing.cantRepeticiones}</p>
                 </div>
