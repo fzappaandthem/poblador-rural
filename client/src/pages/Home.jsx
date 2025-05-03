107a8dafe390b57651899755184171f081ca3788
JayFiji <fkafkaandthem@gmail.com>
2025-05-03 14:43:45 -0300

    [FIXED]primero muestra p2p luego misas [TEST]veo si además muestra el encabezado solo si hay MPRs de esa categoría

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -71,36 +71,33 @@ export default function Home() {
         </h1>
         
 
+
         {listings && listings.length > 0 &&  (
-          
-              <div className="flex flex-col gap-6 ml-auto mr-auto mt-10">
-              <p>Misas</p>
+          <div className="flex w-full flex-col gap-4 autofill:bg-slate-100">
+              {
+                listings.filter(l => l.category === 'p2p' && new Date (l.fechaUltimaEmision) >= fechaEmisionProx.getTime() - 1 * 60 * 1000 && new Date (l.createdAt) <= fechaEmisionProx.getTime() + 15 * 60 * 1000 ).map((listing) => (
+                  <div className="flex flex-col gap-6 ml-auto mr-auto mt-10">
+                <p>Persona a persona</p>
+                  <ListingItem key={listing._id} listing={listing} />
+                </div>
+                ))
+              }
+          </div>
+        )}
+
+        {listings && listings.length > 0 &&  (
                 <div className="flex w-full flex-col gap-4 autofill:bg-slate-100">
                   {
                     
                     listings.filter(l => l.category === 'misas' && new Date (l.fechaUltimaEmision) >= fechaEmisionProx.getTime() - 1 * 60 * 1000 && new Date (l.createdAt) <= fechaEmisionProx.getTime() + 15 * 60 * 1000 ).map((listing) => (
+                      <div className="flex flex-col gap-6 ml-auto mr-auto mt-10">
+                      <p>Misas</p>
                       <ListingItem key={listing._id} listing={listing} />
+                      </div>
                     ))
                   }
-                </div>
               </div>
             )}
-
-        {listings && listings.length > 0 &&  (
-          
-          <div className="flex flex-col gap-6 ml-auto mr-auto mt-10">
-          <p>Persona a persona</p>
-            <div className="flex w-full flex-col gap-4 autofill:bg-slate-100">
-              {
-                
-                listings.filter(l => l.category === 'p2p' && new Date (l.fechaUltimaEmision) >= fechaEmisionProx.getTime() - 1 * 60 * 1000 && new Date (l.createdAt) <= fechaEmisionProx.getTime() + 15 * 60 * 1000 ).map((listing) => (
-                  <ListingItem key={listing._id} listing={listing} />
-                ))
-              }
-            </div>
-          </div>
-        )}
-
         {listings && listings.length > 0 && (
               <div className="flex flex-col gap-6 ml-auto mr-auto mt-10">
 
@@ -115,9 +112,6 @@ export default function Home() {
             )}
 
 
-        {/* listings.map((listing) => (
-                      <div className="w-1/3" >{listing.recipient}</div>
-                    )) */}
       </div>
     </div>
   )
